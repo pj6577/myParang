d7901a2a2342686fc3a2bb8c8fcbee609af92b96
chan <qkrcksgh156@gmail.com>
2022-08-16 17:01:43 +0900

    start_project

---
+++ parang/src/test/java/com/waveway/parang/ParangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waveway.parang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
